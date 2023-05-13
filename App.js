e3c1f29b6269b08c83217aa38bba8f632210a285
thiago-guerra <thiago.cesa@hotmail.com>
2023-05-13 01:19:44 -0300

    Incluindo estilos aos textos e imagem logo

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import { SafeAreaView,  StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar, View } from 'react-native';
 import Cesta from './src/Cestas';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
 export default function App() {
+  const [fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratNegrito": Montserrat_700Bold
+  });
+
+  if (!fontCarregada) {
+    return <View />
+  }
+  
   return (
     <SafeAreaView>
-      <StatusBar/>
+      <StatusBar />
       <Cesta />
     </SafeAreaView>
   );
--- src/Cestas.js
+++ src/Cestas.js
@@ -1,28 +1,77 @@
-import { Image,  StyleSheet, Dimensions, Text} from 'react-native';
+import { Image, StyleSheet, Dimensions, Text, View } from 'react-native';
 import Topo from '../assets/topo.png';
+import LogoFazenda from '../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
-export default function Cesta(){
+export default function Cesta() {
     return <>
-    <Image source={Topo} style={estilos.topo}/>
-    <Text style={estilos.Titulo}>Detalhes da Cesta</Text>
+        <Image source={Topo} style={estilos.topo} />
+        <Text style={estilos.Titulo}>Detalhes da Cesta</Text>
+
+        <View style={estilos.cesta}>
+            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <View style={estilos.fazenda}>
+                <Image style={estilos.imagemFazenda} source={LogoFazenda}></Image>
+                <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+            </View>
+            <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadozamente da fazenda
+                direto para sua cozinha
+            </Text>
+            <Text style={estilos.preco}>R$ 40,00</Text>
+        </View>
     </>
 }
 
 const estilos = StyleSheet.create({
     topo: {
         width: '100%',
-        height: 578 / 768 * width
+        height: 578 / 768 * width,
     },
     Titulo: {
-         position: 'absolute',
-         textAlign: 'center',
-         width: '100%',
-         color: 'white',
-         fontSize: 16,
-         lineHeight: 26,
-         top: 15,
-         fontWeight: 'bold'
+        position: 'absolute',
+        textAlign: 'center',
+        width: '100%',
+        color: 'white',
+        fontSize: 16,
+        lineHeight: 26,
+        top: 15,
+        fontWeight: 'bold'
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16
+    },
+    nome: {
+        fontSize: 26,
+        lineHeight: 42,
+        color: '#464646',
+        fontFamily: 'MontserratNegrito'
+    },
+    fazenda: {
+        flexDirection: 'row',
+        paddingVertical: 12
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: 'MontserratRegular'
+    },
+    descricao: {
+        color: '#a3a3a3',
+        fontSize: 16,
+        lineHeight: 26
+    },
+    preco: {
+        color: '#2a9f85',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8,
+        fontWeight: 'bold'
     }
 });
